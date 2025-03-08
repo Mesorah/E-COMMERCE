ee3fafe521328e43ecc12998393418c0631f675d
Messorah <garabrielra@gmail.com>
2025-03-08 12:19:14 -0300

    mudando os locais dos arquivos para melhor entendimento

--- staff_management/urls.py
+++ staff_management/urls.py
@@ -6,6 +6,7 @@ app_name = 'staff'
 
 urlpatterns = [
      path('', views.HomeListView.as_view(), name='index'),
+
      path('add_product/',
           views.ProductCreateView.as_view(),
           name='add_product'
@@ -17,6 +18,10 @@ urlpatterns = [
      path('product/delete/<int:pk>/',
           views.ProductDeleteView.as_view(),
           name='delete_product'
+          ),
+     path('products/search/',
+          views.StaffSearchListView.as_view(),
+          name="staff_search"
           ),
 
      path('ordered/index/',
@@ -33,6 +38,11 @@ urlpatterns = [
           name='complete_ordered'
           ),
 
+     path('ordereds/search/',
+          views.StaffOrderedSearchListView.as_view(),
+          name="staff_ordered_search"
+          ),
+
      path('clients/',
           views.ClientsListView.as_view(),
           name='clients'
@@ -40,6 +50,10 @@ urlpatterns = [
      path('client/<int:pk>/',
           views.ClientListOrderedDetailView.as_view(),
           name='client_list_ordered'
+          ),
+     path('clients/search/',
+          views.StaffClientsSearchListView.as_view(),
+          name="staff_client_search"
           ),
 
      path('support/staff/',
@@ -57,18 +71,5 @@ urlpatterns = [
      path('support/question/detail/<int:pk>/',
           views.SupportQuestionDetail.as_view(),
           name='support_question_detail'
-          ),
-
-     path('products/search/',
-          views.StaffSearchListView.as_view(),
-          name="staff_search"
-          ),
-     path('ordereds/search/',
-          views.StaffOrderedSearchListView.as_view(),
-          name="staff_ordered_search"
-          ),
-     path('clients/search/',
-          views.StaffClientsSearchListView.as_view(),
-          name="staff_client_search"
           ),
 ]
