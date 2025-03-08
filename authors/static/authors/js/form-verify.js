6c60134fe3762cf801fee0e687b8f5ba5cb2b2d4
Messorah <garabrielra@gmail.com>
2025-03-08 20:26:32 -0300

    corrigindo os bugs da verificação em js

--- authors/static/authors/js/form-verify.js
+++ authors/static/authors/js/form-verify.js
@@ -5,23 +5,33 @@ function main() {
     const form = document.querySelector('.author-form');
     form.addEventListener('submit', function(e) {
         e.preventDefault();
-        getElements();
+        const isValid = getElements()
+
+        if(isValid) form.submit();
     });
 }
 
 function getElements() {
     const elements = document.querySelectorAll('.form-control');
-    validElements(elements);
+    const isValidElements = validElements(elements);
 
     const username = document.querySelector('#id_username');
-    validUsername(username);
+    const isValidUsername = validUsername(username);
 
     const cpf = document.querySelector('#id_cpf');
-    validCPF(cpf);
+    const isValidCPF = validCPF(cpf);
 
     const password1 = document.querySelector('#id_password1');
     const password2 = document.querySelector('#id_password2');
-    validPassword(password1, password2, username);
+    const isValidPassword = validPassword(password1, password2, username);
+
+    return isValid(isValidElements, isValidUsername, isValidCPF, isValidPassword);
+}
+
+function isValid(validElements, validUsername, validCPF, validPassword) {
+    if(validElements && validUsername && validCPF && validPassword) return true;
+
+    return false;
 }
 
 function addError(element, msg) {
@@ -34,13 +44,21 @@ function addError(element, msg) {
 
 function validElements(elemets) {
     const errors = document.querySelectorAll('.error-message');
+    let valid = true;
+
     for(const element of errors) {
         element.remove();
     }
 
     for(const element of elemets) {
-        if(!element.value) addError(element, `O ${element.name} não estar ser vazio`);
+        if(!element.value){
+            addError(element, `O ${element.name} não estar ser vazio`);
+
+            valid = false;
+        }
     }
+
+    return valid;
 }
 
 function validUsername(username) {
@@ -48,7 +66,7 @@ function validUsername(username) {
         username, `O ${username.name} não pode ter mais que 150 caracteres`
     );
 
-    return username.value.length > 150;
+    return username.value.length < 150;
 }
 
 function validCPF(cpf) {
@@ -60,22 +78,51 @@ function validCPF(cpf) {
 }
 
 function validPassword(password1, password2, username) {
-    if(password1.value !== password2) {
+    let valid = true;
+
+    if(password1.value !== password2.value) {
         addError(password1, `A ${password1.name} tem que ser igual a ${password2.name}`);
         addError(password2, `A ${password2.name} tem que ser igual a ${password1.name}`);
+        valid = false;
     }
 
-    if(password1.value.length < 8) addError(
-        password1, `A ${password1.name} tem que ser maior que 8 caracteres`
-    );
+    if(password1.value.length < 8) {
+        addError(
+            password1, `A ${password1.name} tem que ser maior que 8 caracteres`
+        );
 
-    if(typeof password1.value === Number) addError(
-        password1, `A ${password1.name} não pode ser interamente numérica`
-    );
+        valid = false;
+    }
 
-    if(password1.value === username.value) addError(
-        password1, `A ${password1.name} não pode ser igual ao ${username.name}`
-    );
+    if(isNumeric(password1)) valid = false;
+
+    if(password1.value === username.value) {
+        addError(
+            password1, `A ${password1.name} não pode ser igual ao ${username.name}`
+        );
+
+        valid = false;
+    }
+
+    return valid;
+}
+
+function isNumeric(password) {
+    try {
+        const value = Number(password.value);
+        
+        // if you couldn't turn it into a numerical 
+        if(value) {
+            addError(
+                password, `A ${password.name} não pode ser interamente numérica`
+            );
+            
+            return true;
+        }
+        
+    } catch (error) {
+        return false;
+    }   
 }
 
 main();
