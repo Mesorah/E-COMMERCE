046dfe7046ed2947f8d43ac4c1b2a0c795ed259d
Messorah <garabrielra@gmail.com>
2025-04-19 17:23:50 -0300

    configurando para o primeiro teste passe corretamente

--- tests/functional_tests/home/test_home_index.py
+++ tests/functional_tests/home/test_home_index.py
@@ -4,7 +4,7 @@ from tests.functional_tests.base import BaseFunctionalTest
 
 
 class HomeIndexFunctionalTest(BaseFunctionalTest):
-    def test_recipe_home_page_without_recipes_not_found_message(self):
+    def test_home_index_without_products_not_found_message(self):
         self.browser.get(self.live_server_url)
         body = self.browser.find_element(By.TAG_NAME, 'body')
-        self.assertIn('Produtos Disponíveis', body.text)
+        self.assertIn('Nenhum produto disponível', body.text)
