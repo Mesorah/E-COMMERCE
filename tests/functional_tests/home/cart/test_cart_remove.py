3f44a4bff679ac8c843ecd5b2f3fe5d7d5636125
Messorah <garabrielra@gmail.com>
2025-04-20 20:28:29 -0300

    separando os functional testes em módulos próprios

---
+++ tests/functional_tests/home/cart/test_cart_remove.py
@@ -0,0 +1,10 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+from tests.functional_tests.base import BaseFunctionalTest
+from utils.for_tests.base_for_setup import create_product_setup
+
+
+class CartRemoveFunctionalTest(BaseFunctionalTest):
+    def test_the_test(self):
+        assert 1 == 1
