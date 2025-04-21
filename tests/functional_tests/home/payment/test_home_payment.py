d46ae75f91b53c95654fe6d0bd687192ddcc31b0
Messorah <garabrielra@gmail.com>
2025-04-21 18:15:53 -0300

    criando as verificações para o pagamento

--- tests/functional_tests/home/payment/test_home_payment.py
+++ tests/functional_tests/home/payment/test_home_payment.py
@@ -1,10 +1,10 @@
 from django.urls import reverse
 from selenium.webdriver.common.by import By
 
-from tests.functional_tests.base import BaseFunctionalTest
+from tests.functional_tests.home.payment.base import PaymentBaseFunctionalTest
 
 
-class PaymentFunctionalTest(BaseFunctionalTest):
+class PaymentFunctionalTest(PaymentBaseFunctionalTest):
     def setUp(self):
         super().setUp()
 
@@ -12,35 +12,39 @@ class PaymentFunctionalTest(BaseFunctionalTest):
         self.browser.get(self.live_server_url + reverse('home:payment'))
 
         self.informations = {
-            'id_first_name': '',
-            'id_last_name': '',
-            'id_credit_card': '',
-            'id_expiration_date': '',
-            'id_cvv': '',
-            'id_cardholder_name': '',
-            'id_zip_code': '',
-            'id_neighborhood': '',
-            'id_street_name': '',
-            'id_house_number': ''
+            'id_first_name': 'Camila',
+            'id_last_name': 'Oliveira',
+            'id_credit_card': '4242424242424242',
+            'id_expiration_date': '09/2028',
+            'id_cvv': '737',
+            'id_cardholder_name': 'Camila Oliveira',
+            'id_zip_code': '86390000',
+            'id_neighborhood': 'Itaim Bibi',
+            'id_street_name': 'Rua Joaquim Florian',
+            'id_house_number': '450'
         }
 
-    def test_home_payment_works_if_all_data_is_correctly(self):
-        informations_list = [
-            'Camila',
-            'Oliveira',
-            '4242424242424242',
-            '09/2028',
-            '737',
-            'Camila Oliveira',
-            '86390000',
-            'Itaim Bibi',
-            'Rua Joaquim Floriano',
-            '450'
-        ]
+    def submit_payment_form_and_get_errors(
+            self, field_id, field_value, *error_messages
+    ):
+        self.informations[field_id] = field_value
 
-        for field_id, expected_text in zip(
-            self.informations.keys(), informations_list
-        ):
+        for current_field_id, expected_text in self.informations.items():
+            field = self.browser.find_element(By.ID, current_field_id)
+            field.send_keys(expected_text)
+
+        # He send the informations
+        form = self.browser.find_element(By.CLASS_NAME, 'payment-form')
+        form.submit()
+
+        # View a error
+        errors = self.browser.find_elements(By.CLASS_NAME, 'error-message')
+
+        for error_message in error_messages:
+            self.assertIn(error_message, [error.text for error in errors])
+
+    def test_home_payment_works_if_all_data_is_correctly(self):
+        for field_id, expected_text in self.informations.items():
             field = self.browser.find_element(By.ID, field_id)
             field.send_keys(expected_text)
 
@@ -54,6 +58,28 @@ class PaymentFunctionalTest(BaseFunctionalTest):
         ).text
 
         self.assertEqual(cart_count, '0')
+
+    def test_home_payment_credit_card_has_error(self):
+        self.submit_payment_form_and_get_errors(
+            'id_credit_card',
+            '1234567890123456',
+            'Cartão inválido.'
+        )
+
+    def test_home_payment_expiration_date_has_error(self):
+        self.submit_payment_form_and_get_errors(
+            'id_expiration_date',
+            '0931',
+            'Formato de data inválido. Use MM/AA ou MM/YYYY.',
+            'Mês inválido.'
+        )
+
+    def test_home_payment_zip_code_has_error(self):
+        self.submit_payment_form_and_get_errors(
+            'id_zip_code',
+            '12345678',
+            'CEP diferente de cambará'
+        )
 
     def test_home_payment_all_placeholders(self):
         # View the forms placeholders
